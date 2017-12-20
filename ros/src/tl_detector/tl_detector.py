e6761cf57de0b642417dfa45b4225838a882250a
deepblack <mario.landry@gmail.com>
2017-12-20 07:55:28 -0500

    Changed back to ssd

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -5,7 +5,7 @@ from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-from light_classification.tl_classifier_frcnn import TLClassifier
+from light_classification.tl_classifier import TLClassifier
 from copy import copy
 import cv2
 import math
